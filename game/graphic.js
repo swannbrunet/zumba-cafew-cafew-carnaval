09cb92a992beff2c602d509b13e330bf5b18f19f
swannbrunet <swannbrunet@hotmail.fr>
2020-09-30 19:37:01 +0200

    add light

--- game/graphic.js
+++ game/graphic.js
@@ -28,7 +28,7 @@ function init()
     player1 = new Player("player1", 0xffff00, new THREE.Vector2(50, 0), 0);
     scene.add(player1.graphic);
 
-    light1 = new Light("sun", 0xffffff, "0,0,340");
+    light1 = new Light("sun", 0xffffff, "0,0,10");
     scene.add(light1);
 }
 
